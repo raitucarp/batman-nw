e5a64e21e5b7a56caebf4bd8d46f1d68c90d213d
Raitucarp <ribhararnus.pracutiar@gmail.com>
2013-12-08 16:41:45 +0700

    fix /Y confirmation

--- build.bat
+++ build.bat
@@ -3,7 +3,8 @@ cd release && mkdir %1
 cd ..
 cd projects/%1 && 7za a -tzip ../../release/%1/%2.nw . -mx9
 cd ../..
-xcopy tools\source\*.* release\%1
+xcopy tools\source\*.* release\%1  /Y
 cd release/%1 && copy /b nw.exe+%2.nw %2.exe /Y
 del %2.nw
+del nw.exe
 cd ..
